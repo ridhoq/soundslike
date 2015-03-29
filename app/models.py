d414d5d1ce95331eac3e5aa8135100e404646837
Ridwan Hoq <ridwanhoq@gmail.com>
2015-03-29 18:37:33 -0500

    before first migration

---
+++ app/models.py
@@ -0,0 +1,27 @@
+from . import db
+from werkzeug import generate_password_hash, check_password_hash
+from flask.ext.login import UserMixin
+
+class Song(db.Model):
+    __tablename__ = 'songs'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256))
+    url = db.Column(db.String(256), unique=True)
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
--- manage.py
+++ manage.py
@@ -2,18 +2,17 @@
 import os
 from app import create_app, db
 #from app.models import User, Role
-from flask.ext.script import Manager, Shell
-#from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
-#migrate = Migrate(app, db)
+migrate = Migrate(app, db)
 
 ''''
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
 manager.add_command("shell", Shell(make_context=make_shell_context))
-manager.add_command('db', MigrateCommand)
 
 
 @manager.command
@@ -23,6 +22,7 @@ def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 '''
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
