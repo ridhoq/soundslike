9423cd00f24fa061ac264d63adc89148ca8d81b4
Ridwan Hoq <ridwanhoq@gmail.com>
2017-03-25 20:56:33 -0500

    add signup form

--- static/components/login/login.js
+++ static/components/login/login.js
@@ -1,18 +1,61 @@
 import React, {Component} from "react";
 
-export default class LoginForm extends Component {
+export class LoginForm extends Component {
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log("handled submit");
+    }
+
     render() {
         return (
-            <form>
-                <div className="form-group">
-                    <label>Username/Email</label>
-                    <input type="email" className="form-control" placeholder="Username/Email"/>
-                </div>
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Password"/>
-                </div>
-            </form>
+            <div className="col">
+                <h4>Login</h4>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label>Username/Email</label>
+                        <input type="email" className="form-control" placeholder="Username/Email"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input type="password" className="form-control" placeholder="Password"/>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Log in</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export class SignUpForm extends Component {
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log("handled submit");
+    }
+
+    render() {
+        return (
+            <div className="col">
+                <h4>Sign Up</h4>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label>Username</label>
+                        <input type="text" className="form-control" placeholder="Username"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Email (optional)</label>
+                        <input type="email" className="form-control" placeholder="Email"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input type="password" className="form-control" placeholder="Password"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Confirm Password</label>
+                        <input type="password" className="form-control" placeholder="Confirm Password"/>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Log in</button>
+                </form>
+            </div>
         );
     }
 }
