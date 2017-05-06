0ee995492c3366912a845da5b2bd7fbeb8a1e9a7
Ridwan Hoq <ridwanhoq@gmail.com>
2017-05-06 18:32:51 -0500

    use redux-persist to persist auth state

--- client/index.js
+++ client/index.js
@@ -5,27 +5,31 @@ import {BrowserRouter as Router, Route} from "react-router-dom";
 import localforage from "localforage";
 import createHistory from 'history/createBrowserHistory';
 import {Provider} from "react-redux";
-import {createStore} from "redux";
-import throttle from "lodash/throttle";
+import {createStore, compose} from "redux";
+import {persistStore, autoRehydrate} from "redux-persist";
 
 import App from "./containers/app";
 import Home from "./containers/home";
 import LogInFormContainer from "./containers/login";
 import SongSingleContainer from './containers/songs/single';
-import {loadState, saveState} from "./utils/localstorage";
 import soundsLikeReducer from "./reducers";
 
 const mount = document.createElement("div");
-const localForageInstance = localforage.createInstance({name: "soundslike"});
-const persistedState = loadState(localForageInstance);
-const store = createStore(soundsLikeReducer, persistedState);
-const history = createHistory();
 
-store.subscribe(throttle(() => {
-    saveState(localForageInstance, {
-        auth: store.getState().auth
-    });
-}, 1000));
+const store = createStore(
+    soundsLikeReducer,
+    undefined,
+    compose(
+        autoRehydrate()
+    )
+);
+persistStore(store, {
+    whitelist: ['auth'],
+    storage: localforage,
+    keyPrefix: "soundslike-"
+});
+
+const history = createHistory();
 
 const Index = () => (
     <Provider store={store}>
